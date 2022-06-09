64edc9af096cd73d3802a711dc43427a7429d674
Tirtht <tirththakar@gmail.com>
2022-06-09 18:41:22 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mywebappS22',
+    version='',
+    packages=['myapp', 'myapp.migrations', 'mywebappS22'],
+    url='',
+    license='',
+    author='salon',
+    author_email='',
+    description=''
+)
